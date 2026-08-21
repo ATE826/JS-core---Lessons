f096e60a901b49986821b7e0c130031fb173afb5
ToniTsey <antontseysnovskiy@gmail.com>
2026-08-21 13:04:03 +0300

    Js Core: Homework 38

--- Lesson 38 - Лексическое окружение и контекст выполнения/hw.js
+++ Lesson 38 - Лексическое окружение и контекст выполнения/hw.js
@@ -1,38 +1,25 @@
 "use strict";
 
-const form = document.forms["car-form"];
-const inputs = form.querySelectorAll("input");
-const successBtn = form.querySelector(".btn-success");
+const btn = document.querySelector(".btn");
+const alertMessages = document.querySelectorAll(".info");
 
-const list = document.querySelector("ul");
-
-successBtn.onclick = function (event) {
-  event.preventDefault();
-
-  let name = form.elements.name.value;
-  let year = form.elements.year.value;
-  let color = form.elements.color.value;
-
-  if (name.trim() == "" || year == "" || color.trim() == "") {
-    alert("Не все поля заполнены!");
-    return;
-  }
-
-  const car = new Car(name, year, color);
-
-  const li = document.createElement("li");
-  li.classList.add("list-group-item");
-  li.textContent = `Car: ${car.name} | Year: ${car.year} | Color: ${car.color}`;
-  list.append(li);
-
-  form.elements.name.value = "";
-  form.elements.year.value = "";
-  form.elements.color.value = "";
+const user = {
+  name: "Toni Tsey",
+  age: 21,
+  address: "34/35, Palm Beach, Miami, 3456",
 };
 
-// Constructer
-function Car(name, year, color) {
-  this.name = name;
-  this.year = year;
-  this.color = color;
+function getInfo(key) {
+  return this[key];
+}
+
+btn.addEventListener("click", clickHandler);
+
+function clickHandler() {
+  let idx = 0;
+  for (let key in user) {
+    console.log(getInfo.call(user, key));
+    alertMessages[idx].innerText = getInfo.call(user, key);
+    idx++;
+  }
 }
